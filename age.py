41e826eb75d74e2198661d5526fa831301e4d643
Lucas Santos <lukas_santos.silva@hotmail.com>
2022-05-26 13:48:15 -0300

    a conditional on whether you are underage or overage

---
+++ age.py
@@ -0,0 +1,11 @@
+name = str(input('Type your name: '))
+age = int(input('Type your age: '))
+
+if age < 18:
+    print('The is under age')
+else:
+  if age > 65:
+    print('The is elderly')
+  else:
+    age > 18 and age < 64
+    print('The is of legal age')
